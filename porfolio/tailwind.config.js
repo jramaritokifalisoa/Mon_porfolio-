9a3001ee2f01f14e4160dcaad45b700ad6693b38
Ramaritoki fali_isoa <jramaritokifalisoa@gmail.com>
2026-07-22 18:12:07 +0300

    Installation de tout les preé-requis

---
+++ porfolio/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // S'adapte si tu es sur React ou Vue
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
