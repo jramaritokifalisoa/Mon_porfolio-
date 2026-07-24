115e3e2fdbc84da4d3f508cdde1d9758b2dcd79b
Ramaritoki fali_isoa <jramaritokifalisoa@gmail.com>
2026-07-25 00:11:11 +0300

    reset

--- porfolio/src/components/layout/header.tsx
+++ porfolio/src/components/layout/header.tsx
@@ -62,7 +62,7 @@ export const Header = () => {
             </a>
             <a
               href="../public/cv-developpeur.pdf"
-              download="cv-Jocyl_Nardo.pdf"
+              download="cv-developpeur.pdf"
               className="w-full sm:w-auto"
             >
               <Button
